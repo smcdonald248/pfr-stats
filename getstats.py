b1c1c80d312f5c446c0bcf02c04d857f70e8a623
smcdonald248 <smcdonald248@gmail.com>
2020-01-07 14:08:01 -0500

    sqlite db loading and cleanup

--- getstats.py
+++ getstats.py
@@ -6,10 +6,16 @@ scrapes tables from pro-football-reference.com
 12/11/2019 -- Progress so far:
 url scraping and BS4 parsing in place -- can find headers, data and commit them to python lists for simpler manipulation.
 
-High Level things To Do:
-list of urls to scrape... more testing...
-database loading
-analysis
+1/7/2020
+Database loading for 'Advanced Passing' table complete
+Data sanity checks to allow for sqlite data loading (header rows cleaned out, etc.)
+Analysis moved to statsbasic.py to finish out UofM coursework
+
+Roadmap / Ideas:
+Config file -- list of urls to scrape with table name tuple (e.g. url, table_name) 
+    - More testing required to ensure that dynamic table creation will not barf when using different tables
+Class/Def creation -- make this more usable
+
 
 '''
 
@@ -19,6 +25,9 @@ import ssl
 from bs4 import BeautifulSoup
 from time import sleep
 
+conn = sqlite3.connect('stats.sqlite')
+cur = conn.cursor()
+
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
@@ -26,7 +35,11 @@ ctx.verify_mode = ssl.CERT_NONE
 
 url = input("Enter URL: ")
 if len(url) < 1:
-    url = "https://www.pro-football-reference.com/years/2019/opp.htm"
+    url = "https://www.pro-football-reference.com/years/2019/passing_advanced.htm"
+
+table = input("Enter Table Name: ")
+if len(table) < 1:
+    table = "Passing"
 
 html = urllib.request.urlopen(url)
 soup = BeautifulSoup(html, "html.parser")
@@ -47,6 +60,56 @@ for tr in tbody_soup[0].findAll('tr'):
         temp.append(td.getText())
     data.append(temp)
 
-print(column_headers)
-for i in data:
-    print(i)    
+cur.execute('DROP TABLE IF EXISTS ' + table + ';')
+
+x = 0
+col = ''
+for i in column_headers:
+    if x == 0:
+        cur.execute('CREATE TABLE ' + table + '( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE );')
+        try:
+            cur.execute('ALTER TABLE ' + table + ' ADD COLUMN ' + i + ' REAL;')
+        except:
+            cur.execute('ALTER TABLE ' + table + ' ADD COLUMN ' + i + ';')
+        col = col + i + ','
+        x =+ 1
+    else:
+        if i.find('/') != -1:
+            i = i.replace('/', '')
+        try:
+            cur.execute('ALTER TABLE ' + table + ' ADD COLUMN ' + i + ' REAL;')
+        except:
+            cur.execute('ALTER TABLE ' + table + ' ADD COLUMN ' + i + ';')
+        col = col + i + ','
+
+col = col[:-1]
+conn.commit()
+
+x = 0
+fields = ''
+while x < len(data[x]): 
+    fields = fields + '?,' 
+    x += 1
+fields = fields[:-1]
+
+y = 0
+for i in data:    
+    x = 0
+    values = ''
+    while x < len(data[y]): 
+        values = values + '\'' + data[y][x] + '\'' + ','
+        x += 1
+    values = values[:-1]
+    
+    check = values.split(',')
+    if len(check) < len(data[0]):
+        y += 1
+        continue
+    else:        
+        cur.execute('INSERT INTO ' + table + '(' + col + ') Values ( ' + fields + ')', ( eval(values) ))
+        y += 1
+
+print('Done! ' + str(y) + ' records poplated into ' + table)
+
+conn.commit()
+cur.close()
